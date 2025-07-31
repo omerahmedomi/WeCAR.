fff00a999dadca5ce6026830e44c03b2ffa520ff
Umer <omiomeromi11@gmail.com>
2025-07-31 22:34:59 +0300

    Add slide in motion to hero texts with framer motion

--- Client/src/pages/App.jsx
+++ Client/src/pages/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./../components/Header";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (
@@ -9,14 +10,19 @@ const App = () => {
         <div class="relative h-[400px] md:h-[600px] lg:h-screen bg-[url('https://images.unsplash.com/photo-1432462770865-65b70566d673?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDE0fHx8ZW58MHx8fHx8')] bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center">
           <div class="absolute inset-0  bg-black opacity-60 "></div>
 
-          <div class="relative z-10 text-white p-6 font-eczar sm:text-center justify-center">
-            <h1 class="text-5xl md:text-6xl font-bold max-w-200">
+          <motion.div
+            class="relative z-10 text-white p-6 font-eczar sm:text-center justify-center"
+            initial={{ x: -200, opacity:0}}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <h1 class="text-5xl md:text-6xl font-bold max-w-200 ">
               First Class Car Rentals at Unbeatable Prices
             </h1>
             <p class="mt-2 text-lg md:text-xl">
               From economy to luxury, we’ve got the right car for your journey!
             </p>
-          </div>
+          </motion.div>
         </div>
 
         <div>
