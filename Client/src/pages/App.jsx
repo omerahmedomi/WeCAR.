ffa095b123c0f9bda1f753440132416b88ab35ba
Umer <omiomeromi11@gmail.com>
2025-08-06 20:11:57 +0300

    Update achievement section data and numbers

--- Client/src/pages/App.jsx
+++ Client/src/pages/App.jsx
@@ -146,26 +146,26 @@ const App = () => {
           <div className="shadow-2xl flex divide-x-1 py-2 sm:py-5 mt-4 md:mt-6">
             <div className=" pl-2 sm:pl-10 md:pl-15 lg:pl-20 pr-2 sm:pr-5 md:pr-10 lg:pr-12">
               <p className="font-bold text-cyan-500 text-2xl sm:text-3xl 2xl:text-4xl font-grenze">
-                {inView && <CountUp end={4000} duration={3} suffix="+" />}
+                {inView && <CountUp end={350} duration={3} suffix="+" />}
               </p>
               <p className="text-nowrap font-semibold font-eczar text-sm sm:text-base 2xl:text-lg ">
-                Active member
+                Cars Availabe
               </p>
             </div>
             <div className="  px-2 sm:px-5 md:px-10 lg:px-12 ">
               <p className="font-bold text-cyan-500 text-2xl sm:text-3xl 2xl:text-4xl font-grenze">
-                {inView && <CountUp end={4000} duration={3} suffix="+" />}
+                {inView && <CountUp end={10000} duration={3} suffix="+" />}
               </p>
               <p className="text-nowrap font-semibold font-eczar text-sm sm:text-base 2xl:text-lg ">
-                Active member
+                Total Bookings
               </p>
             </div>
             <div className="  pr-2 sm:pr-10 md:pr-15 lg:pr-20   pl-2 sm:pl-5 md:pl-10 lg:pl-12">
               <p className="font-bold text-cyan-500 text-2xl sm:text-3xl 2xl:text-4xl font-grenze">
-                {inView && <CountUp end={4000} duration={3} suffix="+" />}
+                {inView && <CountUp end={5000} duration={3} suffix="+" />}
               </p>
               <p className="text-nowrap font-semibold font-eczar text-sm sm:text-base 2xl:text-lg ">
-                Active member
+              Happy Customers
               </p>
             </div>
           </div>
