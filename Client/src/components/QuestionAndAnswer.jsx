49d0e7e8fc7cb44901019354ce5d7b5bd055ece0
Umer <omiomeromi11@gmail.com>
2025-08-07 19:08:25 +0300

    Add FAQs section component

---
+++ Client/src/components/QuestionAndAnswer.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+import ArrowDown from "./../Icons/ArrowDown";
+
+const QuestionAndAnswer = ({question,answer}) => {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div
+        className="question  bg-gray-200 flex justify-between px-5 py-3 cursor-pointer "
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="font-medium ">{question}</span>
+        <span
+          className={`transform transition-transform duration-500 ${
+            isOpen ? "-rotate-180" : ""
+          }`}
+        >
+          <ArrowDown />
+        </span>
+      </div>
+
+      <div
+        className={`bg-gray-50 overflow-hidden transition-all duration-500 ${
+          isOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+        } px-5`}
+      >
+        <p className="py-3">
+          {answer}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default QuestionAndAnswer
--- Client/src/pages/FAQs.jsx
+++ Client/src/pages/FAQs.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import QuestionAndAnswer from "../components/QuestionAndAnswer";
 
 const FAQs = () => {
+  
+
+ 
   return (
     <div>
-      <Header/>
+      <Header />
+      <main className="w-full max-w-200 mx-auto py-16 px-5 h-svh font-eczar pt-20 divide-y-1 divide-white">
+        <div>
+          <QuestionAndAnswer question="Questions" answer="answer" />
+        </div>
+        <div>
+          <QuestionAndAnswer question="Question" answer="answer" />
+        </div>
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default FAQs
+export default FAQs;
