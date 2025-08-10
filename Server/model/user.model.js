23ea4f4967234b92f66b03d0852733d69638839f
Umer <omiomeromi11@gmail.com>
2025-08-10 08:16:17 +0300

    Add User model schema content

--- Server/model/user.model.js
+++ Server/model/user.model.js
@@ -1,10 +1,37 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-
-
-
-},{timestamps:true})
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 25,
+    },
+    lastName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 25,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 8,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User',userSchema)
 export default User;
