a3659efcb970e7d4765f881f5fbf7a0dac100989
Umer <omiomeromi11@gmail.com>
2025-09-03 20:43:26 +0300

    update and fix theme scipt in head in index.html

--- Client/src/hooks/useTheme.jsx
+++ Client/src/hooks/useTheme.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 export default function useTheme() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
   useEffect(() => {
     if (theme === "dark") {
